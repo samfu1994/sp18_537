daea9aa9d28989f0313a4704b30b494fc22a9a6e
Hao Fu <hfu@royal-01.cs.wisc.edu>
2018-03-03 22:22:39 -0600

    finish

--- p2b/xv6/user/test1.c
+++ p2b/xv6/user/test1.c
@@ -3,6 +3,23 @@
 #include "user.h"
 
 int main(int argc, char * argv[]) {
+    int pid = getpid();
+    int root_pid = getpid();
+    struct pstat * p = (struct pstat*) malloc(sizeof(struct pstat));;
+    getpinfo(p);
+
+    for(int i = 0; i < 3;i++) {
+        if(pid == root_pid){
+            fork();
+        }
+        pid = getpid();
+        if(pid != root_pid) {
+            printf(1, "child is %d\n", pid);
+            exit();
+        }
+    }
+    exit();
+    
     /*struct pstat * p = (struct pstat*) malloc(sizeof(struct pstat));;
     getpinfo(p);
     int pid = getpid();
@@ -14,12 +31,11 @@ int main(int argc, char * argv[]) {
     }
     printf(1, "-------------------\n");
     */
-    settickets(20);
-    getpinfo(NULL);
+    //settickets(20);
+    //getpinfo(NULL);
     /*for(int i = 0; i < 64; i++) {
             printf(1, "%d,  %d,  %d,  %d\n", p -> inuse[i], p -> tickets[i], p -> pid[i], p -> ticks[i]);
     }
     */
 
-    exit();
 }
