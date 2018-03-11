7333580f94fc6167d05e9c902f492c21b5392675
Hao Fu <hfu@snares-08.cs.wisc.edu>
2018-03-10 19:56:03 -0600

    finish p3

--- p3/xv6/user/test1.c
+++ p3/xv6/user/test1.c
@@ -5,11 +5,18 @@
 #include "stat.h"
 #include "user.h"
 #define PGSIZE 4096
+void *allocate_aligned(uint size, uint alignment)
+{
+        const uint mask = alignment - 1;
+            const uint mem = (uint)malloc(size + alignment);
+                return (void *)((mem + mask) & ~mask);
+}
+    
 int
 main(int argc, char *argv[])
 {
   //printf(1, "%s", "** Placeholder program for grading scripts **\n");
-    int n = 1;
+    /*int n = 1;
     char a[PGSIZE * n];
     a[0] = 'k';
     a[1] = 'o';
@@ -17,6 +24,17 @@ main(int argc, char *argv[])
     //munprotect((void*)a, n);
     //a[2] = 'o';
     //a[3] = 'k';
-    printf(1, "%c%c%c%c\n", a[0], a[1], a[2], a[3]);
     exit();
+    */
+     int n = 1;
+         char *a = allocate_aligned(n*PGSIZE, PGSIZE);
+             a[0]='k';
+                 a[1] = 'o';
+                     mprotect(a, n);
+    printf(1, "%c%c%c%c\n", a[0], a[1], a[2], a[3]);
+                munprotect(a,n );
+                     a[2] = '3';
+
+
+                     exit();
 }
